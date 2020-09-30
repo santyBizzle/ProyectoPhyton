2117f2fffc590760b4f1b82372d12e34f4b676b4
byronSanty <bscabezas@uce.edu.ec>
2020-09-30 12:33:05 -0500

    web

--- run_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e 
-
-. ~/venv/bin/activate
-
-PYTHONPATH=. python -m pystache.commands.test
--- run_tests_with_junit.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e 
-
-. ~/.virtualenvs/python2.7/bin/activate
-
-./test.py
